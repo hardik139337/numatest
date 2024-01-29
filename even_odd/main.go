340bafc33dcde1abea39dc5d573b0b52812e3d91
hardik <hardik>
2024-01-29 10:23:21 +0530

    map update

--- even_odd/main.go
+++ even_odd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/numaproj/numaflow-go/pkg/mapper"
@@ -12,6 +13,7 @@ type EvenOdd struct {
 
 func (e *EvenOdd) Map(ctx context.Context, keys []string, d mapper.Datum) mapper.Messages {
 	// msg := d.Value()
+	calculateSumOfSquares(100)
 	_ = d.EventTime() // Event time is available
 	_ = d.Watermark() // Watermark is available
 	// If msg is not an integer, drop it, otherwise return it with "even" or "odd" key.
@@ -25,4 +27,15 @@ func main() {
 	if err != nil {
 		log.Panic("Failed to start map function server: ", err)
 	}
+}
+
+func calculateSumOfSquares(end int) {
+
+
+	sum := 0
+	for i := 0; i <= end; i++ {
+		sum += i * i
+	}
+
+	fmt.Printf("sum: %v\n", sum)
 }
